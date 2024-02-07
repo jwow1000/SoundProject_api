ec049be0dde83d4d4c4ba5caad323f382678ade7
Jeremy Wiles-Young <jwilesyoung@gmail.com>
2024-02-07 10:43:27 -0500

    functionality is good three links working

---
+++ harder_api/README.md
@@ -0,0 +1,7 @@
+
+<h1>reSearch Music with Images</h1>
+<samp>a simple program that interacts with the discogs.com database.</samp>
+
+
+
+
--- harder_api/script.js
+++ harder_api/script.js
@@ -1,11 +1,13 @@
+// api stuff
 const baseUrl = 'https://api.discogs.com';
+const myToken = 'dgoPiRdAagChWVYQqpfTKEYSVAJGpdSpPxuisxkx';
+// global variable for focus
 let imgFocused = false;
 
-const myToken = 'dgoPiRdAagChWVYQqpfTKEYSVAJGpdSpPxuisxkx';
-const printArt1 = document.querySelector('#printArt1');
-const printArt2 = document.querySelector('#printArt2');
+// declare submit page, image area, and search bar
 const theForm = document.querySelector('#submit');
 const imgArea = document.querySelector('#imageArea');
+const userInput = document.querySelector('#search');
 
 const handleSubmit = function (e) {
     e.preventDefault();
@@ -19,7 +21,7 @@ theForm.addEventListener('submit', handleSubmit);
 
 async function getFirstFive(s) {
     console.log(`searching for ${s}`);
-    const response = await fetch(`${baseUrl}/database/search?q=${s}?page=1&per_page=10&token=${myToken}`);
+    const response = await fetch(`${baseUrl}/database/search?q=${s}&type=all&token=${myToken}`);
     const data = await response.json();
 
     // clear image area
@@ -51,6 +53,7 @@ async function getFirstFive(s) {
         //newA.appendChild(nestedNoImg);
         newDiv.appendChild(nestedImg);
 
+
     }
 }
 
@@ -63,11 +66,14 @@ imgArea.addEventListener('click', function (e) {
             const discLinks = document.createElement('a');
             discLinks.innerText = 'discogs link';
             discLinks.setAttribute('id', 'discLink');
+            discLinks.dataset.link = e.target.dataset.link;
             discLinks.dataset.secLink = 'discLink';
+
             // create the reSearch link
             const reSearch = document.createElement('a');
             reSearch.innerText = 'reSearch';
             reSearch.setAttribute('id', 'reSearch');
+            reSearch.dataset.title = e.target.dataset.title;
             reSearch.dataset.secLink = 'reSearch';
 
             // create the youtube link
@@ -75,6 +81,8 @@ imgArea.addEventListener('click', function (e) {
             youTube.innerText = 'link 2 youtube';
             youTube.setAttribute('id', 'youTube');
             youTube.dataset.secLink = 'youTube';
+            youTube.dataset.title = e.target.dataset.title;
+
 
 
             imgArea.appendChild(discLinks);
@@ -84,33 +92,45 @@ imgArea.addEventListener('click', function (e) {
             imgFocused = true;
 
         } else {
-            // find the img with the focusImg
-            e.target.setAttribute('id', '');
-            const delLink = imgArea.querySelectorAll('a');
-            console.log(delLink);
-            for (let i = 0; i < delLink.length; i++) {
-                delLink[i].remove();
-            }
-
-            imgFocused = false;
+            deleteFocus();
         }
 
     }
+    function deleteFocus() {
+        // find the img with the focusImg
+        const focus = document.getElementById('focusImg');
+        focus.setAttribute('id', '');
+        const delLink = imgArea.querySelectorAll('a');
+        console.log(delLink);
+        for (let i = 0; i < delLink.length; i++) {
+            delLink[i].remove();
+        }
+        // set global var to false
+        imgFocused = false;
+    }
 
     if (e.target.dataset.secLink === 'reSearch') {
-        getFirstFive(e.target.dataset.title);
+        userInput.value = e.target.dataset.title;
+        deleteFocus();
+        console.log(e.target);
+        userInput.value = e.target.dataset.title;
     }
     if (e.target.dataset.secLink === 'youTube') {
-        youTube();
+        const point = e.target.dataset.title;
+        console.log(point);
+        const link = `https://www.youtube.com/results?search_query=${point}`;
+        window.open(link, '_blank');
     }
     if (e.target.dataset.secLink === 'discLink') {
-        console.log(e.target.dataset.link);
+        console.log(e.target);
+        window.open(`${e.target.dataset.link}`, '_blank');
+
     }
 });
 
 // function events for the focus links
 function reSearch(s) {
-    getFirstFive(s);
+
 }
 function youTube(s) {
     console.log('take me to youtube');
